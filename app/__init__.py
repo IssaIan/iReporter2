08358215aa9ed12b4ca5b5f8fdacd402ddbaa2ac
IssaIan <issamwangi@gmail.com>
2018-12-06 01:35:36 +0300

    Copied files

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask, Blueprint
 from instance.config import app_config
-
-from .api.v1 import version_one as v1
+from db_config import *
+from .api.v1 import version_two as v2
 
 def create_app(config_name='testing'):
     app = Flask(__name__, instance_relative_config=True)
     app.url_map.strict_slashes = False
     app.config.from_object(app_config['testing'])
     app.config.from_pyfile('config.py')
-    app.register_blueprint(v1)
+    create_tables()
+    app.register_blueprint(v2)
     return app
 
