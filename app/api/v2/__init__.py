195d886cf6e5a21d529a618025aeff0d2f52d404
IssaIan <issamwangi@gmail.com>
2018-12-06 17:47:38 +0300

    Blueprint routes and app creation

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,7 +1,11 @@
 from flask_restful import Api, Resource
 from flask import Blueprint
+from app.api.v2.views.incidentviews import Incidents, Incident
 
 
 
 version_two = Blueprint('api_v2', __name__, url_prefix='/api/v2')
+
 api = Api(version_two)
+api.add_resource(Incidents, '/incidents')
+api.add_resource(Incident, '/incidents/<int:incident_id>')
