01c7d9238b4a369e0aaf25f1c775bffcad5a8d5e
IssaIan <issamwangi@gmail.com>
2018-12-11 01:01:43 +0300

    [finishes #162478967]All incidents records can be retrieved from the db. A specific incident can also be retrieved.

--- app/api/v2/models/incidentmodels.py
+++ app/api/v2/models/incidentmodels.py
@@ -1,5 +1,7 @@
 import psycopg2
+from flask import jsonify, json
 from flask_restful import request
+from psycopg2.extras import RealDictCursor
 from db_config import *
 
 
@@ -7,60 +9,31 @@ class IncidentModels():
     def __init__(self):
         self.incidents = init_db()
 
-    def save_incident(self, incident_id, created_by, typee, description, status, location, created_on ):
+    def save_incident(self, typee, description, status, location):
 
-        curr = self.incidents.cursor()
+        curr = self.incidents.cursor(cursor_factory=RealDictCursor)
         curr.execute(
-        "INSERT INTO incidents(incident_id, created_by, type, description, status, \
-        location, created_on)VALUES(%s, %s, %s, %s, %s, %s, %s)", 
-        (incident_id, created_by, typee, description, status, location, created_on))
+            "INSERT INTO incidents(type, description, status, location)VALUES(%s, %s, %s, %s)",
+            (typee, description, status, location))
         self.incidents.commit()
 
     def get_all(self):
-        curr = self.incidents.cursor()
+        curr = self.incidents.cursor(cursor_factory=RealDictCursor)
         curr.execute("SELECT * FROM incidents")
-        rows = curr.fetchall()
-        incidentlist = []
-        
-        for i, items in enumerate(rows):
-            incident_id, created_by, typee, description, status, location, created_on = items
-            data = dict(
-                incident_id = int(incident_id),
-                created_by = int(created_by),
-                type = typee,
-                description = description,
-                status = status,
-                location = location,
-                created_on = created_on
-            )
-            incidentlist.append(data)
+        incidentlist = curr.fetchall()
         return incidentlist
 
     def find_by_id(self, id):
-        curr = self.incidents.cursor()
-        curr.execute("SELECT FROM incidents WHERE incident_id = (%s);", (id,))
-        row = curr.fetchone()
-        incident = []
-        for items in enumerate(row):
-
-            incident_id, created_by, typee, description, status, location, created_on = items
-            data = dict(
-               incident_id = int(incident_id),
-               created_by = int(created_by),
-               type = typee,
-               description = description,
-               status = status,
-               location = location,
-               created_on = created_on
-            )
-            incident.append(data)
+        curr = self.incidents.cursor(cursor_factory=RealDictCursor)
+        curr.execute(
+            "SELECT * FROM incidents WHERE incident_id = {}".format(id))
+        incident = curr.fetchall()
         return incident
-        
 
     def delete(self, id):
-        curr = self.incidents.cursor()
-        curr.execute("DELETE FROM incidents WHERE incident_id = (%s);",(id,))
+        self.find_by_id(id)
+        curr = self.incidents.cursor(cursor_factory=RealDictCursor)
+        curr.execute("DELETE FROM incidents WHERE incident_id = {}".format(id))
         self.incidents.commit()
-
 
     
