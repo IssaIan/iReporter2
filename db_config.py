b2a1dadc35e54231439d00d5b9cb5a61f349cba9
IssaIan <issamwangi@gmail.com>
2018-12-11 01:29:30 +0300

    [finishes #162563404]Use a RealDictCursor

--- db_config.py
+++ db_config.py
@@ -1,19 +1,24 @@
 import psycopg2
 import os
+from psycopg2.extras import RealDictCursor
 
-url = "dbname='ireporter' host='localhost' port='5432' user='issa' password='Maina9176'"
+
+url = os.getenv('DB_URL')
+
 
 def connection(url):
     conn = psycopg2.connect(url)
     return conn
 
+
 def init_db():
     conn = connection(url)
     return conn
 
+
 def create_tables():
     conn = connection(url)
-    curr = conn.cursor()
+    curr = conn.cursor(cursor_factory=RealDictCursor)
     queries = tables()
 
     for query in queries:
@@ -21,22 +26,21 @@ def create_tables():
 
     conn.commit()
 
-def destroy_tables():
-    pass
 
 def tables():
     users = """CREATE TABLE IF NOT EXISTS users(
         user_id serial PRIMARY KEY NOT NULL,
-        first_name character varying(50) NOT NULL,
-        last_name character varying(50) NOT NULL,
-        username character varying(50) NOT NULL,
-        email character varying(50) NOT NULL,
+        first_name character varying(100) NOT NULL,
+        last_name character varying(100) NOT NULL,
+        username character varying(100) NOT NULL,
+        email character varying(100) NOT NULL,
         date_created timestamp with time zone DEFAULT ('now'::text):: date NOT NULL,
-        password character varying(50) NOT NULL
+        phonenumber int,
+        password character varying(150) NOT NULL
     )"""
     incidents = """CREATE TABLE IF NOT EXISTS incidents (
         incident_id serial PRIMARY KEY NOT NULL,
-        created_by serial NOT NULL,
+        created_by integer references users (user_id),
         type character varying(20) NOT NULL,
         description character varying (200) NOT NULL,
         status character varying(20) DEFAULT 0,
@@ -45,3 +49,12 @@ def tables():
     )"""
     queries = [users, incidents]
     return queries
+
+
+# def destroy_tables():
+#     conn = connection(url)
+#     curr = conn.cursor()
+
+#     curr.execute("DROP TABLE users")
+#     curr.execute("DROP TABLE incidents")
+#     conn.commit()
