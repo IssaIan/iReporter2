a9c9b279d87dd1b7e641f4bbfa65a0e8f8cd5e65
IssaIan <issamwangi@gmail.com>
2018-12-11 18:12:17 +0300

    [finishes #162579976]Add routes for update location and comment

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,6 +1,6 @@
 from flask_restful import Api, Resource
 from flask import Blueprint
-from app.api.v2.views.incidentviews import Incidents, Incident, LocationUpdate
+from app.api.v2.views.incidentviews import Incidents, Incident, LocationUpdate, CommentUpdate
 from app.api.v2.views.userviews import Users, Login
 
 
@@ -11,3 +11,5 @@ api.add_resource(Incidents, '/incidents')
 api.add_resource(Incident, '/incidents/<int:incident_id>')
 api.add_resource(Users, '/users')
 api.add_resource(Login, '/login')
+api.add_resource(LocationUpdate, '/incidents/<int:incident_id>/location')
+api.add_resource(CommentUpdate, '/incidents/<int:incident_id>/comment')
