c609444a2364a722818d77733765b9a615d71844
IssaIan <issamwangi@gmail.com>
2018-12-11 18:28:49 +0300

    [finishes #162580563]Updates

--- instance/config.py
+++ instance/config.py
@@ -5,19 +5,23 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
-    DB_URL = os.getenv('DB_URL')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     DB_URL = os.getenv('DB_URL')
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
-
+    DB_NAME = os.getenv('DB_NAME')
 
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
     DEBUG = True
+    DB_NAME = os.getenv('DBTEST_NAME')
 
 class StagingConfig(Config):
     """Configurations for Staging."""
@@ -28,7 +32,7 @@ class ProductionConfig(Config):
     """Configurations for Production."""
     DEBUG = False
     TESTING = False
-
+    DB_NAME = os.getenv('DB_NAME')
 
 app_config = {
     'development': DevelopmentConfig,
