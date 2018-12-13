a6a76ee3a78345be9809d8da87f6b42e5718427b
IssaIan <issamwangi@gmail.com>
2018-12-13 11:49:07 +0300

    Added function to create superadmin

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import os
 from app import create_app
 from db_config import Db
+from app.api.v2.models.usermodels import UserModels
 
 app = create_app(os.getenv('FLASK_CONFIG'))
 
 with app.app_context():
     Db().init_app(app)
     Db().create_tables()
+    UserModels().create_admin()
     
 if __name__ == '__main__':
     app.run(DEBUG=True)
