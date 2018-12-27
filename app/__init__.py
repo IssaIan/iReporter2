f9dd39a271350e73a751b2b630675fd7659ee94e
IssaIan <issamwangi@gmail.com>
2018-12-27 16:34:08 +0300

    [finishes #162858002] Add flask-cors

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_jwt_extended import JWTManager
 from instance.config import Config
 import datetime
 from flask import Flask, Blueprint
+from flask_cors import CORS
 from instance.config import app_config
 from app.api.v2.routes import version_two as v2
 from db_config import Db
@@ -15,6 +16,7 @@ def create_app(config_name):
     app.register_blueprint(v2)
     app.url_map.strict_slashes = False
     app.config.from_object(app_config[config_name])
+    CORS(app)
     JWTManager(app)
 
     return app
