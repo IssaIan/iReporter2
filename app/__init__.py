2fd4cc9880c12fef35bbb8f09184b241209f573b
IssaIan <issamwangi@gmail.com>
2019-01-01 23:27:30 +0300

    [finishes #162681797] Updates

--- app/__init__.py
+++ app/__init__.py
@@ -13,10 +13,10 @@ from flask import jsonify
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
     app.register_blueprint(v2)
     app.url_map.strict_slashes = False
     app.config.from_object(app_config[config_name])
-    CORS(app)
     JWTManager(app)
 
     return app
