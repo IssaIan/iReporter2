aa1d88850c6685344b03d3f96342b6b00ef62f1f
IssaIan <issamwangi@gmail.com>
2019-01-09 05:57:23 +0300

    Updates

--- UI/js/login.js
+++ UI/js/login.js
@@ -14,15 +14,13 @@ function loginUser(e){
       })
       .then(res => res.json())
       .then(data => {
-        console.log(data[0].User[0].is_admin)
-
         let user = document.getElementById('username').value;
         sessionStorage.setItem('user', user)
         let token = data[0].Token
         sessionStorage.setItem('token', token)
         if (data.Error){
           alert(data.Error)
-          } else if (data[0].User.is_admin === false) {
+          } else if (data[0].User[0].is_admin === false) {
             alert(data[0].Message)
             window.location = "useracc.html"
           } else {
