50f70e96ab3a9b367c4e1febbbb9ad296996ac0f
IssaIan <issamwangi@gmail.com>
2019-02-26 12:04:14 +0300

    Add get user owned incidents functionality

--- UI/js/powerup.js
+++ UI/js/powerup.js
@@ -3,6 +3,7 @@ document.getElementById('type').addEventListener('click', getIncidentsbyType);
 document.getElementById('getstatus').addEventListener('click', getIncidentsbyStatus);
 document.getElementById('findone').addEventListener('click', getIncident);
 document.getElementById('delete').addEventListener('click', deleteIcident);
+document.getElementById('userIncident').addEventListener('click', getUserIncidents);
 
 
 function postIncident(e) {
@@ -254,4 +255,39 @@ function uploadMedia(){
         alert(data.Error)
       }
     })
+}
+
+function getUserIncidents(e){
+  e.preventDefault();
+  let token = sessionStorage.getItem('token');
+  fetch('https://issaireporterv2.herokuapp.com/api/v2/userincidents', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + token
+      }
+      })
+      .then((res) => res.json())
+      .then((data) => {
+          if (data.Error) {
+              alert(data.Error)
+          } else {
+              let output = `<h2>These are all your incidents:</h2>`;
+              data[0].Data.forEach(function (incident) {
+                  output += `
+            <ul class="list-group mb-3">
+                <li class="list-group-item"><strong>INCIDENT ID: </strong>${incident.incident_id}</li>
+                <li class="list-group-item"><strong>CREATED BY: </strong>${incident.created_by}</li>
+                <li class="list-group-item"><strong>CREATED ON: </strong>${incident.created_on}</li>
+                <li class="list-group-item"><strong>STATUS: </strong>${incident.status}</li>
+                <li class="list-group-item"><strong>TYPE OF INCIDENT: </strong>${incident.type}</li>
+                <li class="list-group-item"><strong>DESCRIPTION: </strong>${incident.description}</li>
+                <li class="list-group-item"><strong>LOCATION: </strong>${incident.location}</li>
+                <li class="list-group-item"><img src="${incident.media_path}.png" alt="Incident image" height=auto></li> 
+            </ul>
+            `;
+              });
+              document.getElementById('output0').innerHTML = output;
+              alert(data[0].Message)
+          }
+      })
 }
